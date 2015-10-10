ff1118239a1640f5c0be2d494db02cd12b4d3b14
Andrew Karpushin <reven86@gmail.com>
2015-10-10 18:45:43 +0300

    added libzip-1.0.1a

--- cmake/msvc-amalgamate.bat
+++ cmake/msvc-amalgamate.bat
@@ -1,14 +1,16 @@
 set ROOTDIR=%CD%
 set LIBDIR=%1
+set CURLDIR=%2
 
-echo "Amalgamating target dir: %LIBDIR%"
+echo "Amalgamating target dir: %LIBDIR% curldir: %CURLDIR%"
 
+copy %CURLDIR%\*.lib %LIBDIR:/=\%
 cd %LIBDIR%
+rmdir /s /q "tmp\"
 mkdir tmp
 del gameplay-deps.lib
 move *.lib tmp
 
 LIB.EXE /OUT:gameplay-deps.lib tmp\*
 
-rmdir /s /q "tmp\"
 cd %ROOTDIR%
