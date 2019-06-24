f2e1df4c2877d1af03f0cfa2091b561979ff9fe3
Andrew Karpushin <reven86@gmail.com>
2019-06-24 18:59:15 +0300

    fixed compilation on emscripten

--- protobuf-3.4.0/src/google/protobuf/stubs/atomicops.h
+++ protobuf-3.4.0/src/google/protobuf/stubs/atomicops.h
@@ -63,6 +63,26 @@ namespace google {
 namespace protobuf {
 namespace internal {
 
+
+#ifdef __EMSCRIPTEN__
+#ifdef GOOGLE_PROTOBUF_ARCH_32_BIT
+
+  typedef intptr_t Atomic32;
+  typedef int64 Atomic64;
+#else
+  typedef int32 Atomic32;
+  // We need to be able to go between Atomic64 and AtomicWord implicitly.  This
+  // means Atomic64 and AtomicWord should be the same type on 64-bit.
+  #if defined(__ILP32__) || defined(GOOGLE_PROTOBUF_OS_NACL)
+  // NaCl's intptr_t is not actually 64-bits on 64-bit!
+  // http://code.google.com/p/nativeclient/issues/detail?id=1162
+  // sparcv9's pointer type is 32bits
+  typedef int64 Atomic64;
+  #else
+  typedef intptr_t Atomic64;
+  #endif
+#endif
+#else
 #if defined(GOOGLE_PROTOBUF_ARCH_POWER)
 #if defined(_LP64) || defined(__LP64__)
 typedef int32 Atomic32;
@@ -83,6 +103,7 @@ typedef int32 Atomic32;
 typedef int64 Atomic64;
 #else
 typedef intptr_t Atomic64;
+#endif
 #endif
 #endif
 #endif
