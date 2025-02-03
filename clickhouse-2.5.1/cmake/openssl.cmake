34c85640b13cba22a2f39afe966b650d96b78036
Andrew Karpushin <reven86@gmail.com>
2025-02-03 10:54:48 +0300

    updated ssl to 1.1.1w

--- clickhouse-2.5.1/cmake/openssl.cmake
+++ clickhouse-2.5.1/cmake/openssl.cmake
@@ -1,7 +1,9 @@
 MACRO (USE_OPENSSL)
 
     IF (WITH_OPENSSL)
-        FIND_PACKAGE (OpenSSL REQUIRED)
+        #FIND_PACKAGE (OpenSSL REQUIRED)
+        include_directories("${PROJECT_BINARY_DIR}/../openssl-1.1.1w/include")
+        include_directories("${PROJECT_SOURCE_DIR}/../openssl-1.1.1w/include")
         ADD_COMPILE_DEFINITIONS (WITH_OPENSSL=1)
     ENDIF ()
 
